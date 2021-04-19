095abc44ebe3c058302ca87749195754373a1e91
Brown Mamba <brdyherrera@gmail.com>
2021-04-19 03:23:10 -0400

    set up the functionality for writing data to IndexedDB database

--- public/js/idb.js
+++ public/js/idb.js
@@ -8,7 +8,7 @@ const request = indexedDB.open('budget-tracker', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
+    // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('new_budget', { autoIncrement: true });
     };
 
@@ -19,8 +19,7 @@ request.onsuccess = function(event) {
 
     // check if app is online, if yes run uploadBudget() function to send all local db data to api
     if (navigator.onLine) {
-      // we haven't created this yet, but we will soon, so let's comment it out for now
-      // uploadBudget();
+        uploadBudget();
     }
 };
 
@@ -28,3 +27,15 @@ request.onsuccess = function(event) {
     // log error here
     console.log(event.target.errorCode);
     };
+
+// This function will be executed if we attempt to submit a new budget and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    // access the object store for `new_budget`
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    // add record to your store with add method
+    budgetObjectStore.add(record);
+}
